2bec0ddac906c8ff01372ca125a3a5f7152653e1
fzq <215836017@qq.com>
2017-07-09 22:46:24 +0800

    第二次提交，用来测试AndroidStudio中更新GitHub的步骤

---
+++ MusicPlayer/src/main/java/com/fzq/musicplayer/test/TestGitHub.java
@@ -0,0 +1,12 @@
+package com.fzq.musicplayer.test;
+
+/**
+ * Created by fzq on 2017/7/9.
+ */
+
+public class TestGitHub {
+
+    private void test(){
+        System.out.println("测试提交到GitHub");
+    }
+}
